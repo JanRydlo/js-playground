cc6b781be9d9eaf114a1e4083f30d54f731f9f38
Jan Rydlo <rydlo@datexhk.cz>
2025-10-27 21:17:19 +0100

    Refactors onResolve method

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,23 +6,25 @@ const fileCache = localforage.createInstance({
     name: 'fileCache',
 })
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
+            // Handle root entry file of 'index.js'
+            build.onResolve({ filter: /(^index\.js$)/}, () => {
+                return { path: 'index.js', namespace: 'a' } ;
+            })
+
+            //Handle relative paths in a module
+            build.onResolve({ filter: /^\.+\//}, (args: any) => {
+                return {
+                    namespace: 'a',
+                    path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + "/").href
+                }
+            })
+
+            //Handle main file of the module
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log('onResolve', args);
-                if (args.path === 'index.js') {
-                    return { path: args.path, namespace: 'a' };
-                }
-
-                if (args.path.includes('./') || args.path.includes('../')) {
-                    return {
-                        namespace: 'a',
-                        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + "/").href
-                    }
-                }
-
                 return {
                     namespace: 'a',
                     path: `https://unpkg.com/${args.path}`
@@ -30,23 +32,18 @@ export const unpkgPathPlugin = () => {
 
             });
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log('onLoad', args);
 
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
-                        contents: `
-                          import message from 'axios';
-                          console.log(message);
-                        `,
+                        contents: inputCode,
                     };
                 }
 
                 const cachedItem = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
                 console.log('onLoadDB', cachedItem);
                 if (cachedItem) {
-                    console.log('cached', cachedItem);
                     return cachedItem;
                 }
 
