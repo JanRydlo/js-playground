ca827bd54ca67e649b098649bd2444c354e0a603
Jan Rydlo <rydlo@datexhk.cz>
2025-10-30 20:39:09 +0100

    Adds CellList and CellListItem

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import TextEditor from "./components/text-editor";
 import {Provider} from "react-redux";
 import {store} from "./state";
+import CellList from "./components/cell-list";
 
 const el = document.getElementById("root");
 
@@ -12,7 +12,7 @@ const App = () => {
   return (
       <Provider store={store}>
           <div>
-              <TextEditor />
+              <CellList />
           </div>
       </Provider>
   )
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,22 @@
 import {applyMiddleware, createStore} from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
+import ActionTypes from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+    type: ActionTypes.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type: 'code'
+    }
+})
+
+store.dispatch({
+    type: ActionTypes.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type: 'text'
+    }
+})
